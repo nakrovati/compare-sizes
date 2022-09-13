4bdb7c4b92ce24833d6e3ae045b41252d6cd3239
Daniil Zhuravlev <nakrovati@outlook.com>
2022-09-13 19:06:33 +0300

    chore: add base url for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import { resolve } from "node:path";
 
 export default defineConfig({
+  base: "/compare-sizes/",
   build: {
     rollupOptions: {
       output: {
