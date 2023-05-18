f91869b3cce8dcac874740c12b847321cf4db86e
Daniil Zhuravlev <nakrovati@outlook.com>
2023-05-18 17:18:21 +0400

    chore: chunk pinia into a separate file

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,8 @@ export default defineConfig({
         manualChunks: (id) => {
           if (id.includes("node_modules")) {
             if (id.includes("@vueuse")) return "vendor.@vueuse";
-            if (id.includes("vue")) return "vendor.vue";
             if (id.includes("pinia")) return "vendor.pinia";
+            if (id.includes("vue")) return "vendor.vue";
             if (id.includes("three")) return "vendor.three";
             return "vendor.packages";
           }
