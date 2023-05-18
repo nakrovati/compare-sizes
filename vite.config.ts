34fac937bb4811128c83d9a4ca938ba1ddafabe3
Daniil Zhuravlev <nakrovati@outlook.com>
2023-05-18 16:45:21 +0400

    ci: move site from gh-pages to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vue from "@vitejs/plugin-vue";
 import { resolve } from "node:path";
 
 export default defineConfig({
-  base: "/compare-sizes/",
   build: {
     rollupOptions: {
       output: {
