99b10d3e91bcfa915c7b7d64077807ef81459255
Daniil <nakrovati@outlook.com>
2023-07-01 18:52:04 +0400

    fix: delay when clicking on input

--- src/components/base/BaseInput.vue
+++ src/components/base/BaseInput.vue
@@ -38,8 +38,8 @@ function onInput(event: Event): void {
     position: absolute;
     bottom: 0;
     left: 0.5rem;
-    cursor: text;
-    transition: 0.25s;
+    pointer-events: none;
+    transition: transform 0.25s, font-size 0.25s;
     transform: translateY(-50%);
   }
 
@@ -58,12 +58,12 @@ function onInput(event: Event): void {
     &:focus::placeholder {
       opacity: 1;
     }
-  }
 
-  .base-input__input:not(:placeholder-shown) + .base-input__label,
-  .base-input__input:focus + .base-input__label {
-    font-size: 0.875rem;
-    transform: translateY(-200%);
+    &:not(:placeholder-shown) + .base-input__label,
+    &:focus + .base-input__label {
+      font-size: 0.875rem;
+      transform: translateY(-200%);
+    }
   }
 }
 </style>
